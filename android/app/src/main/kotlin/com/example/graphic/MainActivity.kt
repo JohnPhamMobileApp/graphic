4f9d2e6b330c23ca4ee09a1ca97b66775f79a535
John Pham <johnpham290@yahoo.com>
2024-10-02 19:11:13 -0400

    fading and rotating graphic

---
+++ android/app/src/main/kotlin/com/example/graphic/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.graphic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FadingTextAnimation(),
+    );
+  }
+}
+
+class FadingTextAnimation extends StatefulWidget {
+  @override
+  _FadingTextAnimationState createState() => _FadingTextAnimationState();
+}
+
+class _FadingTextAnimationState extends State<FadingTextAnimation>
+    with SingleTickerProviderStateMixin {
+  bool _isVisible = true;
+  late AnimationController _controller;
+  late Animation<double> _rotationAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    _rotationAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.linear,
+      ),
+    );
+
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        _controller.forward();
+      }
+    });
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void toggleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello, Flutter!'),
+      ),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _isVisible ? 1.0 : 0.0,
+          duration: Duration(seconds: 1),
+          child: AnimatedBuilder(
+            animation: _rotationAnimation,
+            builder: (context, child) {
+              return Transform.rotate(
+                angle: _rotationAnimation.value * 2.0 * 3.14159, // Rotate 360 degrees
+                child: child,
+              );
+            },
+            child: Text(
+              'Hello, Flutter!',
+              style: TextStyle(
+                  fontSize: 24, color: Colors.blue, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: toggleVisibility,
+        child: Icon(_isVisible ? Icons.visibility_off : Icons.visibility),
+      ),
+    );
+  }
+}
